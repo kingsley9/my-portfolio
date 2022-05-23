07f30c1002c96beeaf4ee26a9de22723fda31fc7
kingsley <king808@my.yorku.ca>
2022-05-23 14:40:46 -0400

    Firebase updates

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
-import firebase from 'firebase';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,7 +16,11 @@ const firebaseConfig = {
   appId: "1:445530984843:web:f2580f3607493dd89e9f8e",
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// Use this to initialize the firebase App
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-export default firebase;
+// Use these for db & auth
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth, db };
