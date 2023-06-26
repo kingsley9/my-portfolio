5c1a18347f3ca0453dea2420a586c7de9f145d30
kingsley <king808@my.yorku.ca>
2023-06-26 14:02:57 -0400

    redesign skills section

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,53 +1,53 @@
-import "./skills.scss";
-import Pll from "react-pll";
-import { Typography } from "@mui/material";
+import React from 'react';
+
+import './skills.scss';
+import Pll from 'react-pll';
+import { Typography } from '@mui/material';
 function Skills() {
   return (
     <div className="skills" id="skills">
       {/* Skills container */}
       <Typography
         gutterBottom
-        fontFamily={"Montserrat"}
+        fontFamily={'Montserrat'}
         align="center"
-        variant="h4"
+        variant="h3"
         component="div"
+        fontWeight={'bold'}
       >
         Skills
       </Typography>
       <div className="skills-box">
-        <div className="left">
-          <Pll alt="Python Logo" height={80} language="python" />
+        <Pll alt="Python Logo" height={100} language="python" />
 
-          <Pll alt="Python Logo" height={80} language="java" />
-          <Pll alt="Python Logo" height={80} language="javascript" />
-          <Pll alt="Python Logo" height={80} language="typescript" />
-          <Pll alt="Python Logo" height={80} language="html" />
-          <Pll alt="Python Logo" height={80} language="css" />
-        </div>
-        <div className="right">
-          <Pll alt="Python Logo" height={80} language="c" />
-          <Pll alt="Python Logo" height={80} language="cpp" />
-          <i
-            style={{ fontSize: 90 }}
-            class="devicon-nodejs-plain-wordmark colored"
-          ></i>
-          <i
-            style={{ fontSize: 90 }}
-            class="devicon-react-original-wordmark colored"
-          ></i>
-          <i
-            style={{ fontSize: 90 }}
-            class="devicon-git-plain-wordmark colored"
-          ></i>
-          <i
-            style={{ fontSize: 90 }}
-            class="devicon-mysql-plain-wordmark colored"
-          ></i>
-          <i
-            style={{ fontSize: 100 }}
-            class="devicon-tailwindcss-original-wordmark colored"
-          ></i>
-        </div>
+        <Pll alt="Python Logo" height={100} language="java" />
+        <Pll alt="Python Logo" height={100} language="javascript" />
+        <Pll alt="Python Logo" height={100} language="typescript" />
+        <Pll alt="Python Logo" height={100} language="html" />
+        <Pll alt="Python Logo" height={100} language="css" />
+        <Pll alt="Python Logo" height={100} language="c" />
+        <Pll alt="Python Logo" height={100} language="cpp" />
+        <Pll alt="Python Logo" height={100} language="ruby" />
+        <i
+          style={{ fontSize: 100 }}
+          class="devicon-nodejs-plain-wordmark colored"
+        ></i>
+        <i
+          style={{ fontSize: 100 }}
+          class="devicon-react-original-wordmark colored"
+        ></i>
+        <i
+          style={{ fontSize: 100 }}
+          class="devicon-git-plain-wordmark colored"
+        ></i>
+        <i
+          style={{ fontSize: 100 }}
+          class="devicon-mysql-plain-wordmark colored"
+        ></i>
+        <i
+          style={{ fontSize: 100 }}
+          class="devicon-tailwindcss-original-wordmark colored"
+        ></i>
       </div>
     </div>
   );
